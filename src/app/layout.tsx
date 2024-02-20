445163567ab5925858c8975e20a169dea75cd332
Matheus <matheusmendesreis@outlook.com>
2024-02-20 17:21:30 -0300

    feat:header da page pronto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { SvgSearch } from '@/components/icons/search'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,16 +18,18 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${inter.className} px-4`}>
-        <header className='mx-auto mb-5 mt-9 flex min-h-11 max-w-[1440px] flex-col items-center justify-start gap-5 bg-red-600 py-2 min-[400px]:flex-row'>
-          <h1 className=''>maristore</h1>
-          <div className='max-w-80'>
+        <header className='mx-auto mb-5 mt-9 flex min-h-11 max-w-[1440px] flex-col items-center justify-start gap-5 py-2 min-[400px]:flex-row'>
+          <h1 className='text-xl font-extrabold'>maristore</h1>
+          <label className='flex max-w-80 items-center justify-center gap-3 rounded-3xl bg-zinc-900 px-5 py-3'>
+            <SvgSearch />
             <input
               type='text'
               name=''
               id=''
-              className='w-full'
+              className='w-4/5 bg-transparent outline-none'
+              placeholder='buscar produto...'
             />
-          </div>
+          </label>
         </header>
         <main>{children}</main>
       </body>
