4335a599b9bdb2f8d6e7fb3a3c764d2044f78892
Matheus <matheusmendesreis@outlook.com>
2024-02-22 15:06:48 -0300

    fix:refactor search page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/features/header'
+import { Suspense } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +22,9 @@ export default function RootLayout({
         className={`${inter.className} mx-auto min-h-screen max-w-[1068px] px-4`}
       >
         <Header />
-        <main>{children}</main>
+        <Suspense fallback={<p>Loading feed...</p>}>
+          <main>{children}</main>
+        </Suspense>
       </body>
     </html>
   )
