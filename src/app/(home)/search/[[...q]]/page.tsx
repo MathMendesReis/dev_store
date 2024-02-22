e29e69895bb1a9b88b41a4a87af49222119bf5d4
Matheus <matheusmendesreis@outlook.com>
2024-02-22 11:18:21 -0300

    fix:refactor seaarch page

--- src/app/(home)/search/[[...q]]/page.tsx
+++ src/app/(home)/search/[[...q]]/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import ListProducts from '@/features/list-products'
+import { searchProducts } from '@/mock/search-product'
+import React, { Suspense } from 'react'
 
-export default function Search() {
+interface SearchProps {
+  searchParams: {
+    q: string
+  }
+}
+export default async function Search({ searchParams }: SearchProps) {
+  const { q: query } = searchParams
+  const data = await searchProducts(query)
   return (
-    <div>
-      <p>hello world</p>
+    <div className='flex w-full flex-col gap-4'>
+      <p className='text-sm'>
+        Resultados para: <span className='font-semibold'>{query}</span>
+      </p>
+
+      <div className='grid grid-cols-1 gap-4 min-[964px]:grid-cols-2 min-[1068px]:grid-cols-3'>
+        <Suspense fallback={<p>Loading feed...</p>}>
+          <ListProducts data={data} />
+        </Suspense>
+      </div>
     </div>
   )
 }
