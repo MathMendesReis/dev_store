7f73a2012472bc2abff61785a83970d8a8872016
Matheus <matheusmendesreis@outlook.com>
2024-02-22 13:47:59 -0300

    fix:refactor search page

--- src/features/search-form.tsx
+++ src/features/search-form.tsx
@@ -18,7 +18,7 @@ export function SearchForm() {
   }
   return (
     <form
-      onChange={handleSearch}
+      onSubmit={handleSearch}
       className='flex w-[320px] items-center gap-3 rounded-full bg-white px-5 py-3 ring-zinc-700 dark:bg-zinc-900'
     >
       <SvgSearch className='h-5 w-5 text-zinc-500' />
